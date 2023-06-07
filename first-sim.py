d3013c72f38d75203661f43484c4b89e8eea2ea8
BabaYaga840 <104863253+BabaYaga840@users.noreply.github.com>
2023-06-07 16:12:57 +0530

    Add files via upload

--- first-sim.py
+++ first-sim.py
@@ -13,25 +13,44 @@ import carla
 import random
 import time
 import numpy as np
-import cv2
 import math
 
 IM_WIDTH = 640
 IM_HEIGHT = 480
-initial_d=20
+initial_d=10
 
 
-def process_img(image):
-    i = np.array(image.raw_data)
-    i2 = i.reshape((IM_HEIGHT, IM_WIDTH, 4))
-    i3 = i2[:, :, :3]
-    cv2.imshow("", i3)
-    cv2.waitKey(1)
-    return i3/255.0
+def getd(v1,v2):
+    tf = v1.get_transform()
+    V = tf.get_forward_vector()
+    xf=V.x
+    yf=V.y
+    zf=V.z
+    V=tf.location
+    x1=V.x*xf
+    y1=V.y*yf
+    z1=V.z*zf
+    tf = v2.get_transform()
+    V=tf.location
+    x2=V.x*xf
+    y2=V.y*yf
+    z2=V.z*zf
+    d=((x1-x2)+(y1-y2)+(z1-z2))/math.sqrt(xf**2+yf**2+zf**2)
+    return 2
+
+
+def get_target_signal():
+	a=np.zeros((2,2))
+	a[0][0]=0.1
+	return a
+
+def get_follower_signal(t_sig):
+	return t_sig
 
 
 actor_list = []
 try:
+  while True:
     client = carla.Client('localhost', 2000)
     client.set_timeout(2.0)
 
@@ -49,23 +68,28 @@ try:
     # vehicle.set_autopilot(True)  # if you just wanted some NPCs to drive.
 
     # get current vehicle transform to get both location & rotation
-    tf = target.get_tranform()
+    tf = target.get_transform()
     # Spawn new vehicle with some distance addition to above transformation location (say 10meter in x, y)
-    (x,y,z)=tf.rotation.get_forward_vector()
+    V=tf.rotation.get_forward_vector()
+    x=V.x
+    y=V.y
+    z=V.z
     d=math.sqrt(x*x+y*y+z*z)
-    tf.rotation = tf.location + carla.Location(-initial_d*x/d, -initial_d*y/d, -initial_d*z/d+10)
+    tf.location = tf.location + carla.Location(-initial_d*x/d, -initial_d*y/d, 10)   
     follower = world.spawn_actor(bp, tf)
     #world.spawn(new_vehicle, carla.Transform(new_loc, carla.Rotation())
 
 
     actor_list.append(target)
     actor_list.append(follower)
-
-    t_sig=get_target_signal()
-    f_sig=get_follower_signal(t_sig)
-    target.apply_control(carla.VehicleControl(throttle=t_sig[0][0], steer=t_sig[0][1]))
-    follower.apply_control(carla.VehicleControl(throttle=f_sig[0][0], steer=f_sig[0][1]))
-
+    
+    spectator=world.get_spectator()
+    transform=follower.get_transform()
+    spectator.set_transform(carla.Transform(transform.location+carla.Location(z=50),carla.Rotation(pitch=-90)))
+    actor_list.append(spectator)
+    time.sleep(50)
+    d=getd(target,follower)
+    print(d)
 
     time.sleep(50)
 
