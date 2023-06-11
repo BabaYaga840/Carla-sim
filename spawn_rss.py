6a5567fcd17db9d779fe7ce4b2325f487eab5e7a
BabaYaga840 <104863253+BabaYaga840@users.noreply.github.com>
2023-06-11 19:07:34 +0530

    Create spawn_rss.py

---
+++ spawn_rss.py
@@ -0,0 +1,131 @@
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+import carla
+
+import random
+import time
+import numpy as np
+import math
+
+velocity=20
+tau=1
+num=3
+
+def getdec(v):
+    return 5
+
+def get_maxacc(v):
+    return 10
+
+def getrss(V1):
+    v1=V1.get_velocity()
+    v2=v1
+    t=V1.get_transform().get_forward_vector()
+    v1=(v1).dot(t)/np.sqrt((t).dot(t))
+    v2 = (v2).dot(t) / np.sqrt((t).dot(t))
+    a2c=get_maxacc(v2)
+    l1=V1.bounding_box.extent.x*2
+    a1=getdec(v1)
+    a2=getdec(v2)
+    if a1>=a2:
+        return (a2c/2)*(a2c/a2+1)*(tau**2)+v2*(a2c/a2+1)*tau+(v2**2/(2*a2)-v1**2(2*a1))+l1
+    else:
+        if tau>=(v1-v2)/(a2c+a1) and tau<=(v1*a2/a1-v2)/(a2c+a2):
+            return (v2-v1)*tau+(tau**2)*(a1+a2c)/2+(v1-v2-(a1+a2c)*tau)**2/(2*(a2-a1))+l1
+        else:
+            return a2c*(a2c/a2+1)*(tau**2)/2+v2*(a2c/a2+1)*tau+((v2**2)/(2*a2)-(v1**2)/(2*a1))+l1
+
+
+
+def getd(v1, v2):
+    tf = v1.get_transform()
+    V = tf.get_forward_vector()
+    xf = V.x
+    yf = V.y
+    zf = V.z
+    V = tf.location
+    x1 = V.x * xf
+    y1 = V.y * yf
+    z1 = V.z * zf
+    tf = v2.get_transform()
+    V = tf.location
+    x2 = V.x * xf
+    y2 = V.y * yf
+    z2 = V.z * zf
+    d = ((x1 - x2) + (y1 - y2) + (z1 - z2)) / math.sqrt(xf ** 2 + yf ** 2 + zf ** 2)
+    return d
+
+class spawn:
+    def __init__(self,num,V,sp):
+        self.num=num
+        self.V=V
+        self.sp=sp
+        self.list=[]
+        self.list.append(self.V)
+        self.V.set_target_velocity(velocity)
+    def next(self):
+        if self.num>0:
+            vec = self.sp.rotation.get_forward_vector()
+            rss_d=getrss(self.V)
+            self.sp.location = self.sp.location + carla.Location(-rss_d * vec.x / np.sqrt((vec)np.dot(vec)), -rss_d * vec.y / np.sqrt((vec)np.dot(vec)), 1)
+            follower = world.spawn_actor(bp, self.sp)
+            self.V=follower
+            self.V.set_target_velocity(velocity)
+            self.list.append(self.V)
+            actor_list.append(follower)
+            self.num=self.num-1
+            self.next()
+    def get_list(self):
+        return self.list
+
+
+
+
+
+
+actor_list = []
+try:
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(2.0)
+    world = client.get_world()
+    world = client.load_world('Town05')
+
+    blueprint_library = world.get_blueprint_library()
+
+    bp = blueprint_library.filter('model3')[0]
+    print(bp)
+
+    spawn_point = world.get_map().get_spawn_points()[190]
+
+    target = world.spawn_actor(bp, spawn_point)
+
+    actor_list.append(target)
+    obj=spawn(num,target,spawn_point)
+    spawn.next()
+    list=spawn.get_list()
+    spectator = world.get_spectator()
+    actor_list.append(spectator)
+    while True:
+
+        
+        for i in range(num):
+            list[i].apply_control(carla.VehicleControl(brake=1, steer=0))
+            transform = list[0].get_transform()
+            spectator.set_transform(
+            carla.Transform(transform.location + carla.Location(z=200), carla.Rotation(pitch=-90)))
+            time.sleep(tau)
+
+
+finally:
+    print('destroying actors')
+    for actor in actor_list:
+        actor.destroy()
